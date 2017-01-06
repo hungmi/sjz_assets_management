bba9c983dff414baf1a19163133616b0ac79b8b9
hungmi <hungmihungmi@gmail.com>
2017-01-06 14:06:19 +0800

    remove ajax-loading.js

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,4 @@
 module ApplicationHelper
-  def loader(message=nil)
-    capture do
-      content_tag :div, id: "ajax-appetizer" do
-        concat content_tag :h2, message if message.present?
-        concat image_tag "loader#{rand(1..2)}.gif", class: "img-fluid", style: "max-width: 300px;"
-      end
-    end
-  end
-
   # 取消 turbolinks 緩存，建議用在有 js 套件的頁面
   def no_cache
     content_for :custom_head do
